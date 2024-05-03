2ae0db0d3cb3116b1d353e22bfaa36eed264116e
KatieZeldaKat <katherine@queerkat.games>
2024-05-02 20:43:58 -0500

    Create window format

--- src/objects/InventionTab.ts
+++ src/objects/InventionTab.ts
@@ -1,30 +1,202 @@
-import * as inventionList from "../objects/InventionList";
-import { horizontal, vertical, listview, button, store, WritableStore } from "openrct2-flexui";
 import { Invention } from "./Invention";
+import * as inventionList from "../objects/InventionList";
+import {
+    horizontal,
+    vertical,
+    listview,
+    button,
+    store,
+    WritableStore,
+    label,
+    compute,
+    tab,
+} from "openrct2-flexui";
 
-export function tabContent(category: "all" | "scenery" | RideResearchCategory) {
-    let image = store(inventionList.getInvented(category)[0].previewImage);
+const tabImageMap: { [category: string]: number | ImageAnimation } = {
+    all: { frameBase: 5327, frameCount: 8, frameDuration: 2 },
+    transport: { frameBase: 5537, frameCount: 5, frameDuration: 4 },
+    gentle: { frameBase: 5542, frameCount: 4, frameDuration: 8 },
+    rollercoaster: { frameBase: 5546, frameCount: 5, frameDuration: 2 },
+    thrill: 5562, // Needs custom animation, as ImageAnimation doesn't plug-and-play
+    water: { frameBase: 5551, frameCount: 6, frameDuration: 4 },
+    shop: { frameBase: 5530, frameCount: 7, frameDuration: 4 },
+    scenery: 5459,
+};
+
+const categoryMap: { [category: string]: string } = {
+    transport: "Transport Rides",
+    gentle: "Gentle Rides",
+    rollercoaster: "Roller Coasters",
+    thrill: "Thrill Rides",
+    water: "Water Rides",
+    shop: "Shops & Stalls",
+    scenery: "Scenery & Theming",
+};
+
+export function inventionTab(category: "all" | "scenery" | RideResearchCategory) {
+    const lockSelection = store(false);
+    const selected = store(inventionList.getInvented(category)[0]);
+    return tab({
+        image: tabImageMap[category],
+        content: [tabContent(category, lockSelection, selected)],
+        onOpen: () => {
+            lockSelection.set(false);
+            selected.set(inventionList.getInvented(category)[0]);
+        },
+    });
+}
+
+function tabContent(
+    category: "all" | "scenery" | RideResearchCategory,
+    lockSelection: WritableStore<boolean>,
+    selected: WritableStore<Invention>,
+) {
+    const image = compute(selected, (invention) => invention.previewImage);
+
     return horizontal([
         vertical([
-            createListView(inventionList.getInvented(category), image),
-            createListView(inventionList.getUninvented(category), image),
+            label({ text: "{WHITE}Items pre-invented at start of game:", padding: 0 }),
+            createListView(inventionList.getInvented(category), lockSelection, selected),
+            label({ text: "{WHITE}Items to invent during game:", padding: { top: 5 } }),
+            createListView(inventionList.getUninvented(category), lockSelection, selected),
         ]),
-        button({
-            height: 112,
-            width: 112,
-            image: image,
+        vertical({
+            spacing: 0,
+            content: [
+                button({
+                    height: 110,
+                    width: 110,
+                    padding: { top: "20%", bottom: 5, left: 40 },
+                    image: image,
+                }),
+                label({
+                    text: compute(selected, (invention) => `{WHITE}${invention.type}`),
+                    width: 190,
+                    alignment: "centred",
+                    padding: { left: 2, bottom: 0 },
+                }),
+                label({
+                    text: compute(selected, (invention) => `{BLACK}${invention.name}`),
+                    width: 190,
+                    alignment: "centred",
+                    padding: { left: 2, top: 0 },
+                }),
+                label({
+                    text: compute(
+                        selected,
+                        (invention) =>
+                            `{WHITE}Research Group: {BLACK}${categoryMap[invention.category]}`,
+                    ),
+                    width: 190,
+                    padding: [8, 2],
+                }),
+                horizontal({
+                    padding: { left: 55 },
+                    content: [
+                        createSquareButton("arrow_up", () => {}),
+                        vertical({
+                            spacing: 1,
+                            padding: { bottom: 10 },
+                            content: [
+                                createArrowButton("▲", () => {}),
+                                createArrowButton("▼", () => {}),
+                            ],
+                        }),
+                        createSquareButton("arrow_down", () => {}),
+                    ],
+                }),
+                createListButton(
+                    "Shuffle",
+                    store(inventionList.getUninvented(category).length == 0),
+                    () => {},
+                ),
+                createListButton(
+                    "Invent All",
+                    store(inventionList.getUninvented(category).length == 0),
+                    () => {},
+                ),
+                createListButton(
+                    "Uninvent All",
+                    store(inventionList.getInvented(category).length == 0),
+                    () => {},
+                ),
+            ],
         }),
     ]);
 }
 
-function createListView(inventionList: Invention[], image: WritableStore<number>) {
+function createListView(
+    inventionList: Invention[],
+    lockSelection: WritableStore<boolean>,
+    selected: WritableStore<Invention>,
+) {
     return listview({
         canSelect: true,
-        height: 150,
+        height: "50%",
+        padding: 0,
         columns: [{ header: "Type" }, { header: "Object" }],
         items: inventionList.map((invention) => [invention.type, invention.name]),
         onHighlight: (item) => {
-            image.set(inventionList[item].previewImage);
+            if (!lockSelection.get()) {
+                selected.set(inventionList[item]);
+            }
+        },
+        onClick: (item) => {
+            lockSelection.set(true);
+            selected.set(inventionList[item]);
         },
     });
+}
+
+function createSquareButton(image: number | IconName, onClick: () => void) {
+    return button({
+        image: image,
+        height: 25,
+        width: 25,
+        onClick: onClick,
+    });
+}
+
+function createArrowButton(text: string, onClick: () => void) {
+    return button({
+        text: text,
+        height: 12,
+        width: 25,
+        padding: 0,
+        disabled: true,
+        onClick: onClick,
+    });
+}
+
+function createListButton(
+    text: string,
+    disabled: WritableStore<boolean>,
+    onClick: () => void,
+) {
+    return button({
+        text: text,
+        height: 15,
+        width: 170,
+        padding: [1, 10],
+        disabled: disabled,
+        onClick: onClick,
+    });
+}
+
+function selectInvention(category: "all" | "scenery" | RideResearchCategory) {
+    let inventedItems = inventionList.getInvented(category);
+    let uninventedItems: Invention[];
+    let selectedInvention: Invention | undefined;
+    if (inventedItems.length > 0) {
+        selectedInvention = inventedItems[0];
+    } else {
+        uninventedItems = inventionList.getUninvented(category);
+        if (uninventedItems.length > 0) {
+            selectedInvention = uninventedItems[0];
+        } else {
+            selectedInvention = undefined;
+        }
+    }
+
+    return selectedInvention;
 }
--- src/window/window.ts
+++ src/window/window.ts
@@ -1,6 +1,6 @@
 import * as flex from "openrct2-flexui";
 import * as inventionList from "../objects/InventionList";
-import { tabContent } from "../objects/InventionTab";
+import { inventionTab } from "../objects/InventionTab";
 
 let window: flex.WindowTemplate;
 let isWindowOpen = false;
@@ -9,45 +9,21 @@ export function initialize() {
     inventionList.load();
     window = flex.tabwindow({
         title: "Invention Manager",
-        width: 500,
-        height: "auto",
+        width: { value: 600, min: 400, max: 1200 },
+        height: { value: 400, min: 300, max: 800 },
         position: "center",
         colours: [flex.Colour.LightPurple, flex.Colour.Grey, flex.Colour.Grey],
         onOpen: () => (isWindowOpen = true),
         onClose: () => (isWindowOpen = false),
         tabs: [
-            flex.tab({
-                image: { frameBase: 5327, frameCount: 8, frameDuration: 2 },
-                content: [tabContent("all")],
-            }),
-            flex.tab({
-                image: { frameBase: 5537, frameCount: 5, frameDuration: 4 },
-                content: [tabContent("transport")],
-            }),
-            flex.tab({
-                image: { frameBase: 5542, frameCount: 4, frameDuration: 8 },
-                content: [tabContent("gentle")],
-            }),
-            flex.tab({
-                image: { frameBase: 5546, frameCount: 5, frameDuration: 2 },
-                content: [tabContent("rollercoaster")],
-            }),
-            flex.tab({
-                image: 5562, // Needs custom animation, as ImageAnimation doesn't plug-and-play
-                content: [tabContent("thrill")],
-            }),
-            flex.tab({
-                image: { frameBase: 5551, frameCount: 6, frameDuration: 4 },
-                content: [tabContent("water")],
-            }),
-            flex.tab({
-                image: { frameBase: 5530, frameCount: 7, frameDuration: 4 },
-                content: [tabContent("shop")],
-            }),
-            flex.tab({
-                image: 5459,
-                content: [tabContent("scenery")],
-            }),
+            inventionTab("all"),
+            inventionTab("transport"),
+            inventionTab("gentle"),
+            inventionTab("rollercoaster"),
+            inventionTab("thrill"),
+            inventionTab("water"),
+            inventionTab("shop"),
+            inventionTab("scenery"),
         ],
     });
 }
